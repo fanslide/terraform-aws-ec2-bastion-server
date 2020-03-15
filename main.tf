d9b72b1fdc88dd63c0786b27e64f4d71c625a005
Joseph Boiteau <josephboiteau@gmail.com>
2020-03-16 10:51:11 +1100

    Open Outbound traffic from the bastion

--- main.tf
+++ main.tf
@@ -63,6 +63,13 @@ resource "aws_security_group" "default" {
     }
   }
 
+  egress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   lifecycle {
     create_before_destroy = true
   }
